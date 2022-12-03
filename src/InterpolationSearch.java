546734fb489207b30c2cbd365fb406bd682c98f2
Raccoonee <93836398+Raccoonee@users.noreply.github.com>
2022-12-03 16:00:24 -0600

    feat: add new algorithms

---
+++ src/InterpolationSearch.java
@@ -0,0 +1,13 @@
+public class InterpolationSearch {
+    // improvement over binary search best used for "uniformly" distributed data
+    // "guesses" where a value might be based on calculated probe results
+    // if probe is incorrect, search area is narrowed, and a new probe is calculated
+
+    // average case: O(log(log(n)))
+    // worse case: O(n) [values increase exponentially]
+    public static void main(String[] args) {
+
+
+        
+    }
+}
